2922360db35b1249d36fd18860b7e5ce8361b02d
Sakin790 <sakinislam79@gmail.com>
2024-05-17 21:02:46 +0600

    fresh Project

--- CatFact/src/App.vue
+++ CatFact/src/App.vue
@@ -1,29 +1,6 @@
-<script>
-import { mapState, mapActions } from 'vuex';
-
-export default {
-
-
-
-  computed: {
-    ...mapState({
-      data: state => state.api.data, // Map state properties
-      loading: state => state.api.loading || "data",
-      error: state => state.api.error,
-    }),
-  },
-  methods: {
-    ...mapActions('api', ['fetchData']), // Map actions
-  },
-};
-
-</script>
-
 <template>
-  <div v-if="loading">Loading...</div>
-  <div v-else-if="error">Error: {{ error.message }}</div>
-  <div v-else>
-    <p>{{ data.fact }}</p>
+  <div>
+    <h1>No Way Left..</h1>
+    <p>I Have to ....</p>
   </div>
-  <button @click="fetchData">Fetch Data</button>
 </template>
--- CatFact/src/main.js
+++ CatFact/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import store from './Store/store.js'
-createApp(App).use(store).mount('#app')
+
+createApp(App).mount('#app')
