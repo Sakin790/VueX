52823a33cfefc821b554c01fbbb7fb7455c984dd
Sakin790 <sakinislam79@gmail.com>
2024-05-18 10:04:28 +0600

    working

--- CatFact/src/App.vue
+++ CatFact/src/App.vue
@@ -1,19 +1,48 @@
-<template>
-  <div>
-    <Todo />
-    <input type="text"> <button>Add</button>
-  </div>
-</template>
-
-
 <script>
 import Todo from './components/Todo.vue';
 
-
 export default {
+
+  data() {
+    return {
+      bookName: ""
+    }
+  },
   components: {
     Todo
+  },
+
+  computed: {
+    books() {
+      return this.$store.state.bookList
+    },
+  },
+
+  methods: {
+    addBook() {
+      this.$store.dispatch("addBook", this.bookName)
+
+    },
+    deleteTodo(index) {
+      console.log(index);
+      this.$store.dispatch("deleteTodo", index)
+    }
   }
 
 }
+
 </script>
+
+
+<template>
+  <div>
+
+    <input v-model="bookName" type="text">
+    <button @click="addBook">Add</button>
+    <ul>
+      <li v-for=" (book, index) in books" :key="index">{{ book }}
+        <button @click="deleteTodo(index)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
--- CatFact/src/Store/index.js
+++ CatFact/src/Store/index.js
@@ -1,9 +1,27 @@
 import { createStore } from 'vuex'
 
 export const store = createStore({
-    state() {
-        return {
-            count: 0
+    state: {
+        bookList: ["Sherlok", "Feluda", "Bomkesh", "Daroga"],
+        count: 0
+    },
+
+    actions: {
+        addBook({ commit }, data) {
+            commit("ADD_BOOK", data)
+        },
+        deleteTodo({ commit }, data) {
+            commit("DELETE_TODO", data)
         }
-    }
+    },
+
+    mutations: {
+        ADD_BOOK(state, data) {
+            state.bookList.push(data)
+        },
+        DELETE_TODO(state, data) {
+            state.bookList.splice(data, 1);
+        }
+    },
+
 })
