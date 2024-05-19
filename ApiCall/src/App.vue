4bd064ecdaded4eaf7b2d904b604566c0173cdff
Sakin790 <sakinislam79@gmail.com>
2024-05-19 13:16:38 +0600

    working

--- ApiCall/src/App.vue
+++ ApiCall/src/App.vue
@@ -1,17 +1,10 @@
-<template>
-  <div>
-    <div v-if="loading">Loading...</div>
-    <div v-if="error">{{ error.message }}</div>
-    <ul v-else>
-      <li v-for="post in posts" :key="post.id"> {{ post.id }}-{{ post.title }}</li>
-    </ul>
-  </div>
-</template>
+
 
 <script>
 import { mapState, mapActions } from 'vuex';
 
 export default {
+
   name: 'Posts',
   computed: {
     ...mapState(['posts', 'loading', 'error'])
@@ -24,3 +17,13 @@ export default {
   }
 };
 </script>
+
+<template>
+  <div>
+    <div v-if="loading">Loading...</div>
+    <div v-if="error">{{ error.message }}</div>
+    <ul v-else>
+      <li v-for="post in posts" :key="post.id"> {{ post.views }}-{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
